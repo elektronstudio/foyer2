fdd846dff4db672dff6fba79841c7878454eb59a
kristjanjansen <kristjan.jansen@gmail.com>
2020-10-26 13:53:48 +0100

    cleanup

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 import { createApp, ref } from "./src/deps/vue.js";
 import ThreeScene from "./src/components/ThreeScene.js";
-import ThreeRect from "./src/components/ThreeRect.js";
+import ThreeGeometry from "./src/components/ThreeGeometry.js";
 
 const App = {
-  components: { ThreeScene, ThreeRect },
+  components: { ThreeScene, ThreeGeometry },
   setup() {
     const a = ref(1);
     return { a };
   },
   template: `
   <three-scene>
-    <three-rect :rotation="{ x: a, y: a, z: a}" />
+    <three-geometry geometry="PlaneGeometry" :rotation="{ x: a, y: a, z: a}" />
   </three-scene>
   <input style="position: fixed; top: 10px; left: 10px;" type="range" min="1" max="180" step="0.001" v-model="a" /> -->
 `,
