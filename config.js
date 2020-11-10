f9305f6a88cd0c9c5e887ddb65636ae7372fec49
kristjanjansen <kristjan.jansen@gmail.com>
2020-11-10 22:42:25 +0100

    adjust throttle

--- config.js
+++ config.js
@@ -2,4 +2,4 @@ export const chatUrl = "wss://ws-fggq5.ondigitalocean.app";
 
 //export const chatUrl = "ws://localhost:8080";
 
-export const websocketThrottle = 50;
+export const websocketThrottle = 100;
